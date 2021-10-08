5a828407e861f22f6b864f55dc120560fd1c9cf6
alexa-atremova <77694102+alexa-atremova@users.noreply.github.com>
2021-10-08 10:32:34 +0300

    Add files via upload

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,15 +1,48 @@
 import React from 'react';
 import './Dialogs.css';
+import { NavLink } from 'react-router-dom';
 
-const Profile = () => {
+
+const DialogsItem = (props) => {
+  let path = "/dialogs/" + props.id;
+
+  return <div className="Contacts">
+
+    <NavLink to={path} >{props.name}</NavLink>
+  </div>
+}
+
+const MessageItem = (props) => {
+
+  return <div className="dialog"><span>{props.message}</span></div>
+
+}
+
+
+const Dialogs = () => {
   return (
-    <div className="messages">
-      <div className="yourContacts">
-        Dialog
-       
-      </div>
-    </div>
+    <div className="messagePage">
+      <div className="messages">
 
+        <div className="yourContacts">
+
+          <DialogsItem name="Димон" id="1" />
+          <DialogsItem name="Саня" id="2" />
+          <DialogsItem name="Катюха" id="3" />
+
+        </div>
+
+        <div className="yourDialog">
+          <MessageItem message="Привет!" />
+          <MessageItem message="эй" />
+          <MessageItem message="шо?" />
+          <div>
+
+          </div>
+        </div>
+
+      </div >
+    </div>
   )
 }
-export default Profile;
+export default Dialogs;
