0ea21adf941f76ecbdc3541b9105e96443f3fab9
alexa-artemova <alexandra.artemova1998@gmail.com>
2021-10-11 11:12:32 +0300

    upd map in post

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import './MyPosts.css';
-import Posts  from './Posts/Posts';
+import Posts from './Posts/Posts';
 
-const MyPosts = () => {
+const MyPosts = (prors) => {
+
+
+  let postItem = [
+    { post: "Привет!", like: "3" },
+    { post: "Эй?'", like: "1" },
+    { post: ":(", like: "6" }
+  ]
+
+  let postElements = postItem.map(p => <Posts message={p.post} like = {p.like} />)
+
 
   return (
 
     <div className='posts'>
 
-      <div>My posts
+      <div>my posts
         <div>
           <input type="text" placeholder="Your News..." />
-          <button type="submit"> Send</button>
+          <button type="submit">Send</button>
         </div>
-       <Posts message = 'Привет!' like = '5'/>
-       
-       <Posts message = 'Охаё!' like = '2'/>
-       
+
+        {postElements}
+
       </div>
 
     </div>
